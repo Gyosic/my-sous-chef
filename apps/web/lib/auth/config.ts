256ce3db10476cee9dae310694c5448def731e82
gyosic <gyosic@gractor,com>
2026-03-13 15:24:46 +0900

    feat: next-auth 구성

---
+++ apps/web/lib/auth/config.ts
@@ -0,0 +1,57 @@
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import type { NextAuthConfig } from "next-auth";
+import { SignJWT } from "jose";
+import Google from "next-auth/providers/google";
+import Naver from "next-auth/providers/naver";
+import { accounts, db, users } from "@repo/db";
+
+export type NextAuthPageSearchParams = Promise<{ callbackUrl?: string }>;
+
+export interface Credentials {
+  email: string;
+  password: string;
+}
+
+const secret = new TextEncoder().encode(process.env.AUTH_SECRET);
+
+const authConfig: NextAuthConfig = {
+  session: { strategy: "jwt" },
+  adapter: DrizzleAdapter(db, {
+    usersTable: users,
+    accountsTable: accounts,
+  }),
+  providers: [
+    Google({}),
+    Naver({}),
+  ],
+  pages: {
+    signIn: "/",
+    signOut: "/signout",
+    error: "/error",
+  },
+  callbacks: {
+    jwt: async ({ token }) => {
+      return token;
+    },
+    session: async ({ session, token }) => {
+      const access_token = await new SignJWT({
+        sub: token.sub,
+        id: token.id,
+        email: token.email,
+        name: token.name,
+        role: token.role,
+      })
+        .setProtectedHeader({ alg: "HS256" })
+        .setIssuedAt()
+        .setExpirationTime("1d")
+        .sign(secret);
+
+      return Object.assign(session, { access_token });
+    },
+    signIn: async ({ user }): Promise<boolean> => Boolean(user.id),
+    authorized: async ({ auth }): Promise<boolean> => Boolean(auth),
+  },
+  secret: process.env.AUTH_SECRET,
+};
+
+export default authConfig;
