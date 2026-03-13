a74979a28d7c1682507a7132f9424ba8e6d2703b
gyosic <gyosic@gractor,com>
2026-03-13 17:35:19 +0900

    feat: 로그인 화면 구성

---
+++ apps/web/app/signin/SigninForm.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { signIn } from "next-auth/react";
+import { GoogleButton } from "@repo/ui/shared/GoogleButton";
+import { NaverButton } from "@repo/ui/shared/NaverButton";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@repo/ui/components/card";
+// import { FieldModel } from "@/types";
+import { useTheme } from "next-themes";
+
+export function SigninForm() {
+  const oauthSignin = async (provider: "google" | "naver") => {
+    await signIn(provider);
+  };
+
+  const { theme } = useTheme();
+
+  const naverImgSrc =
+    theme === "dark"
+      ? "/icons/naver-icon-dark.png"
+      : "/icons/naver-icon-green.png";
+
+  return (
+    
+        <Card>
+          <CardHeader className="text-center">
+            <CardTitle className="text-xl">Welcome!</CardTitle>
+            <CardDescription>소셜 로그인으로 빠르게 시작하세요</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <GoogleButton
+              type="button"
+              text="구글로 시작하기"
+              variant="outline"
+              onClick={() => oauthSignin("google")}
+            />
+            <NaverButton
+              type="button"
+              text="네이버로 시작하기"
+              onClick={() => oauthSignin("naver")}
+              buttonImg={naverImgSrc}
+            />
+          </CardContent>
+        </Card>
+    
+  );
+}
---
+++ apps/web/app/signin/page.tsx
@@ -0,0 +1,18 @@
+import { redirect } from 'next/navigation';
+import { auth } from '@/lib/auth';
+import { SigninForm } from './SigninForm';
+
+export default async function SigninPage() {
+  // 이미 로그인되어 있으면 홈으로 리다이렉트
+  const session = await auth();
+
+  if (session) redirect('/');
+
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
+      <div className="flex  flex-col gap-6">
+        <SigninForm />
+      </div>
+    </div>
+  );
+}
