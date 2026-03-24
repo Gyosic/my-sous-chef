5899a85e5eb48f5def14386600ee4e7fdd7566f3
gyosic <gyosic@gractor,com>
2026-03-24 10:08:21 +0900

    chore: recipes meta 정보 추가

--- packages/db/src/types/recipes.ts
+++ packages/db/src/types/recipes.ts
@@ -1,15 +1,32 @@
 import z from "zod";
 
 const stepSchema = z.object({
-  title: z.string("필수입력값 입니다.").min(1, "필수입력값 입니다."),
-  description: z.string("필수입력값 입니다.").min(1, "필수입력값 입니다."),
+  title: z
+    .string("필수입력값 입니다.")
+    .min(1, "필수입력값 입니다.")
+    .meta({ name: "타이틀", type: "text" }),
+  description: z
+    .string("필수입력값 입니다.")
+    .min(1, "필수입력값 입니다.")
+    .meta({ name: "설명", type: "textarea" }),
 });
 const ingredientSchema = z.object({
-  name: z.string("필수입력값 입니다.").min(1, "필수입력값 입니다."),
-  amount: z.string("필수입력값 입니다.").min(1, "필수입력값 입니다."),
-  optional: z.boolean(),
+  name: z
+    .string("필수입력값 입니다.")
+    .min(1, "필수입력값 입니다.")
+    .meta({ name: "재료명", type: "text" }),
+  amount: z
+    .string("필수입력값 입니다.")
+    .min(1, "필수입력값 입니다.")
+    .meta({ name: "계량", type: "text", placeholder: "1 스푼 or 15g" }),
+  optional: z.boolean().meta({ name: "선택", type: "boolean" }),
 });
-const unitSchema = z.object({ name: z.string(), unit: z.string() });
+const unitSchema = z.object({
+  name: z
+    .string()
+    .meta({ name: "계량명", type: "text", placeholder: "1 스푼" }),
+  unit: z.string().meta({ name: "단위", type: "text", placeholder: "15g" }),
+});
 
 export const recipeSchema = z.object({
   name: z
@@ -20,12 +37,16 @@ export const recipeSchema = z.object({
     .string()
     .optional()
     .meta({ name: "레시피 설명", type: "textarea" }),
-  steps: z.array(stepSchema).meta({ name: "요리순서", type: "nested" }),
-  ingredients: z.array(ingredientSchema).meta({ name: "재료", type: "nested" }),
+  steps: z
+    .array(stepSchema)
+    .meta({ name: "요리순서", type: "nested", element: stepSchema.shape }),
+  ingredients: z
+    .array(ingredientSchema)
+    .meta({ name: "재료", type: "nested", element: ingredientSchema.shape }),
   units: z
     .array(unitSchema)
     .optional()
-    .meta({ name: "계량단위", type: "nested" }),
+    .meta({ name: "계량단위", type: "nested", element: unitSchema.shape }),
   servings: z.number().meta({ name: "인분", type: "number" }),
 });
 
